e73dc9f5a99a3c1749fc960579176a5bff764ca8
Роман Орловский <r.orlovskiy@sports.ru>
2023-03-23 13:57:49 +0300

    Game component

---
+++ src/components/Game.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const score = ref(0)
+</script>
+
+<template>
+  <div>Score: {{ score }}</div>
+  <div ref="game"></div>
+</template>
+
+<style scoped>
+</style>
