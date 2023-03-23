534735b82b55fb356f83d7141957607efe866238
Роман Орловский <r.orlovskiy@sports.ru>
2023-03-23 22:11:10 +0300

    Add ball

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import ballImg from '../assets/ball.png';
 
 const score = ref(0)
 </script>
 
 <template>
   <div>Score: {{ score }}</div>
-  <div ref="game"></div>
+  <div ref="game">
+    <img :src="ballImg">
+  </div>
 </template>
 
 <style scoped>
