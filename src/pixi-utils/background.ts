59ad0dc37c70af66829bce0296e8912aead8ef4a
Роман Орловский <r.orlovskiy@sports.ru>
2023-03-26 18:35:00 +0300

    Background as class

--- src/pixi-utils/background.ts
+++ src/pixi-utils/background.ts
@@ -3,21 +3,22 @@ import getPixiApp from "./getPixiApp";
 
 class Background {
   item: PIXI.Sprite;
+  pixiApp: PIXI.Application;
 
   constructor() {
-    const pixiApp = getPixiApp();
     const backTexture = PIXI.Texture.from('/images/background.svg');
+
+    this.pixiApp = getPixiApp();
     this.item = new PIXI.Sprite(backTexture);
     this.updateSizes();
-    pixiApp.stage.addChild(this.item);
+    this.pixiApp.stage.addChild(this.item);
   }
 
   updateSizes() {
-    const pixiApp = getPixiApp();
     this.item.x = 0;
     this.item.y = 0;
-    this.item.width = pixiApp.screen.width;
-    this.item.height = pixiApp.screen.height;
+    this.item.width = this.pixiApp.screen.width;
+    this.item.height = this.pixiApp.screen.height;
   }
 }
 
